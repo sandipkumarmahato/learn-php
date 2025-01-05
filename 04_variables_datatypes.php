3fa77964970e686a1f763f75b256c2c0cc9a4f28
sandipkumarmahato <brandifysolution@gmail.com>
2025-01-05 17:04:01 +0530

    Variables and Datatypes

---
+++ 04_variables_datatypes.php
@@ -0,0 +1,20 @@
+<?php
+    $x = 99;    // Integer
+    $y = "Sandip Kumar Mahato";    // String
+    $z = 99.99; // Float
+    $a = false;  // Boolean
+    $b = array(1,2,3);  // Array
+    $c = null;  // NULL
+
+    var_dump($x);
+    echo "<br>";
+    var_dump($y);
+    echo "<br>";
+    var_dump($z);
+    echo "<br>";
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+?>
